58064773b4233e849f3482bcd23cd824ad77d638
Ioana-Chiriac <ioana.chiriac1014@gmail.com>
2024-03-18 20:26:38 +0200

    ce am reusit sa fac

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log(`hello`);
+// acesare dupa Id
+const cardElemet = document.getElementById(`card`);
+console.log(`card: `, cardElemet);
+console.log(`Card: ${cardElemet}`);
+
+// acesare dupa clasa
+const todos = document.getElementsByClassName(`todo-item`);
+console.log(todos);
+console.log(``);
+
+//acesare dupa nume 
+
+
+
+
